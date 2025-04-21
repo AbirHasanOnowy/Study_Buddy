67422f26f6400a264af9b271910548611e7eceab
abir <abir196600@gmail.com>
2025-04-21 22:03:52 +0600

    Initial commit of Django project structure with base app, models, views, forms, templates, and migrations.

---
+++ studybud/base/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+# Register your models here.
+
+from .models import Room, Topic, Message # Import Room, Topic and Message models
+
+admin.site.register(Room) # Register Room model to admin pannel.
+admin.site.register(Topic) # Register Topic model to admin pannel.
+admin.site.register(Message) # Register Message model to admin pannel.
